ef63371907f84bca868194902a3745f9aefc9b6d
DuridMing <50489088+DuridMing@users.noreply.github.com>
2021-01-18 14:55:51 +0800

    time check

--- consumer.py
+++ consumer.py
@@ -1,10 +1,19 @@
 from kafka import KafkaConsumer
+import time
 
 topics = ['thread0','thread1','thread2','thread3','thread4']
 topic = ['testTopic']
 
 consumer = KafkaConsumer( bootstrap_servers=['localhost:9092'], value_deserializer = lambda v : v.decode('utf-8'))
-consumer.subscribe(topics)
+consumer.subscribe(topic)
+
+num = 0
+avt = 0
 
 for msg in consumer:
-    print(msg)
+    num +=1
+    st = int(time.time()*1000) % 86400000
+    rt = int(msg.value)
+    # print(st -rt)
+    avt = ((st-rt) +avt)/num
+    print("av : %.3f"%avt)
--- producer.py
+++ producer.py
@@ -1,11 +1,13 @@
 from kafbasic import Producer
+import time
 
 producer = Producer()
-producer.producer_set(host=['localhost:9092'] ,topics="testTopic")
+producer.producer_set(host=['localhost:9092'] )
+
 
 
 while True :
-    mes = input("message : ")
-
-    res = producer.send(message=mes , react=True)
-    print(res)
+    st = int(time.time()*1000) % 86400000
+    mes = str(st)
+    print(mes)
+    producer.send(topics="testTopic" ,message=mes, react=False)
