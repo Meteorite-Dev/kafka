2cc75a57b6444b9f728f116e2ba99190fc07d0da
MingChe <ned89212@gmail.com>
2021-01-25 14:45:15 +0800

    send image

--- consumer.py
+++ consumer.py
@@ -1,19 +1,34 @@
 from kafka import KafkaConsumer
 import time
+import cv2
+import numpy as np
+
 
 # topics = ['thread0','thread1','thread2','thread3','thread4']
 topic = ['testTopic']
 
-consumer = KafkaConsumer( bootstrap_servers=['localhost:9092'], value_deserializer = lambda v : v.decode('utf-8'))
+consumer = KafkaConsumer( bootstrap_servers=['node04:9092'])
 consumer.subscribe(topic)
 
 num = 0
 avt = 0
 
 for msg in consumer:
+    image_bytes = msg.value
+    decoded = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
+
     num +=1
     st = int(time.time()*1000) % 86400000
-    rt = int(msg.value)
+    rt = int(msg.key.decode('utf-8'))
     # print(st -rt)
     avt = ((st-rt) +avt)/num
     print("av : %.3f"%avt)
+
+
+    cv2.imshow("rec", decoded)
+    if cv2.waitKey(1) == 27:
+        cv2.destroyAllWindows()
+        print("stop")
+        break
+
+
--- producer.py
+++ producer.py
@@ -1,13 +1,38 @@
-from kafbasic import Producer
+# from kafbasic import Producer
 import time
+import cv2 
+from kafka import KafkaProducer
 
-producer = Producer()
-producer.producer_set(host=['node04:9092'] )
+# ser = lambda V:V.tobytes()
 
+# producer = Producer()
+# producer.producer_set(host=['node04:9092'] ,serializer=ser)
+producer = KafkaProducer(bootstrap_servers='node04:9092',max_request_size=32000000)
 
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+print('ipcam started!')
 
 while True :
+    # print(status)
+    status, Frame = capture.read()
+    buffer = cv2.imencode('.jpg', Frame)[1]
+    
+    res , buffer = cv2.imencode('.jpg', Frame)
     st = int(time.time()*1000) % 86400000
+    cv2.imshow('Image', Frame)
     mes = str(st)
     print(mes)
-    producer.send(topics="testTopic" ,message=mes, react=False)
+    producer.send("testTopic" ,value=buffer.tobytes() , key=mes.encode('utf-8'))
+    if cv2.waitKey(1) == 27:
+        cv2.destroyAllWindows()
+        print("stop")
+        break
+    time.sleep(0.02)
+
+
+# while True :
+#     st = int(time.time()*1000) % 86400000
+#     mes = str(st)
+#     print(mes)
+#     producer.send(topics="testTopic" ,message=mes, react=False)
