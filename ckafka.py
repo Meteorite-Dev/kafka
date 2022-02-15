b5869388cd8b7f603568f77bd0e06b1561438b86
Durid_Ming <50489088+DuridMing@users.noreply.github.com>
2022-02-15 16:33:08 +0800

    add ckafka.py

---
+++ ckafka.py
@@ -0,0 +1,81 @@
+from confluent_kafka import Consumer , Producer
+import cv2 
+from string import Template
+import numpy as np 
+
+"""
+kafka consumer 
+yeild need test (Feb. 15)
+
+need :
+json consumer
+""" 
+class cKafka_Consumer():
+    def __init__(self, server_ip ,port ,topics):
+        self.server_ip = server_ip
+        self.port = port
+        self.topics = topics
+        self.group_id =0
+        self.consumer = self.setting()
+    
+    def setting(self):
+        kafkaserver = Template("${serveraddr}:${port}")
+        server = kafkaserver.substitute(serveraddr=self.server_ip , port=self.port)
+        consumer = Consumer({
+                'bootstrap.servers': server ,
+                'group.id': self.group_id,
+                'auto.offset.reset': 'earliest'
+            })
+
+        consumer.subscribe(self.topics)
+    
+        return consumer
+    
+    def image_Consumer(self) :
+        # receive image using cv2
+        while True:
+            msg = self.consumer.poll(1)
+            
+            if msg is None:
+                continue
+            if msg.error():
+                print("Consumer error: {}".format(msg.error()))
+                continue
+            # return image in while loop
+            image_bytes = msg.value()
+            yield cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
+
+        
+    def test_Consumer(self):
+        while True:
+            msg = self.consumer.poll(1)
+
+            if msg is None:
+                continue
+            if msg.error():
+                print("Consumer error: {}".format(msg.error()))
+                continue
+            yield msg.value().decode('utf-8')
+
+    def close(self):
+        self.consumer.close()
+        
+        
+"""
+kafka producer 
+
+need :
+image producer
+test producer
+json producer 
+"""
+class cKafka_Producer():
+    def __init__(self) -> None:
+        pass                
+        
+    
+
+            
+            
+    
+    
