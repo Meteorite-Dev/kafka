eacb0f44816297e81a34d4854da6ec9e62d01447
Durid_Ming <50489088+DuridMing@users.noreply.github.com>
2022-02-21 22:14:22 +0800

    update r. s. img

--- simg.py
+++ simg.py
@@ -1,13 +1,44 @@
 from ckafka import cKafka_Producer
-
+import argparse
 import cv2
-
 from time import sleep
 
-producer = cKafka_Producer(server_ip="localhost" ,port=9094 ,topics='testTopic')
 
-image = cv2.imread("../0001.jpg")
+def get_parser():
+    parser = argparse.ArgumentParser(description="Simg")
+    parser.add_argument("--image", nargs='?')
+    parser.add_argument("--video", nargs='?')
+    parser.add_argument("--topic" ,default="testTopic" ,nargs=1)
 
-producer.image_Producer(message=image ,topic='testTopic' , t=True)
-sleep(5)
+    return parser
+
+def _frame_from_video(video):
+    while video.isOpened():
+        success, frame = video.read()
+        if success:
+            yield frame
+        else:
+            break
+
+if __name__ == "__main__":
+    producer = cKafka_Producer(server_ip="localhost" ,port=9094 ,topics='testTopic')
+    args = get_parser().parse_args()
+    
+    if args.image:
+        image = cv2.imread(args.image[0])
+
+        producer.image_Producer(message=image ,topic='testTopic' , t=True)
+        sleep(3)
+    elif args.video:
+        video = cv2.VideoCapture(args.video_input)
+        width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        frames_per_second = video.get(cv2.CAP_PROP_FPS)
+        frame = _frame_from_video(video)
+        for cnt, frame in enumerate(frame):
+            producer.image_Producer(message=frame , topics=args.topic[0], t=True)
+    else:
+        print("need image or video args.")
+        raise 
+    
 
